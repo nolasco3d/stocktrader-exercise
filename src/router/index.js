b9dbfe034deafe703feb55cc6709424e83d9373f
André Nolasco <nolasco3d@gmail.com>
2021-12-28 00:24:28 -0300

    Change structure of project

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,22 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/portfolio",
+    name: "Portfolio",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ "../views/Portfolio.vue"),
+  },
+  {
+    path: "/acoes",
+    name: "Acoes",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Acoes.vue"),
   },
 ];
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h3>Negocie e Consule suas Ações</h3>
+
+        <v-alert dense type="info">
+          <h5>Alerta 1</h5>
+        </v-alert>
+
+        <v-alert dense type="info" color="success">
+          <h5>Alerta 2</h5>
+        </v-alert>
+
+        <v-divider></v-divider>
+
+        <h3>Seu Saldo: R$ {{ retornaSaldo }}</h3>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      saldo: 10000,
+    };
+  },
+  computed: {
+    retornaSaldo() {
+      return this.saldo;
+    },
   },
 };
 </script>
